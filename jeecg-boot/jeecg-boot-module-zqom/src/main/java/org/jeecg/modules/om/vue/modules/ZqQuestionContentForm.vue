0876a1e2ae0d4e5652f966572c59f3f8b32645d4
hunji <guo194194@gmail.com>
2020-08-12 18:37:26 +0800

    添加基本的问题填写页面

---
+++ jeecg-boot/jeecg-boot-module-zqom/src/main/java/org/jeecg/modules/om/vue/modules/ZqQuestionContentForm.vue
@@ -0,0 +1,188 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <j-form-container :disabled="formDisabled">
+      <a-form :form="form" slot="detail">
+        <a-row>
+          <a-col :span="24">
+            <a-form-item label="提出人" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['proposer']" placeholder="请输入提出人"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="简要描述" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-textarea v-decorator="['brief']" rows="4" placeholder="请输入简要描述"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="详情" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <j-editor v-decorator="['content',{trigger:'input'}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="解决方案" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <j-editor v-decorator="['solution',{trigger:'input'}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="是否解决" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input-number v-decorator="['solutionState']" placeholder="请输入是否解决" style="width: 100%"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="问题类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['typeId', validatorRules.typeId]" placeholder="请输入问题类型"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col v-if="showFlowSubmitButton" :span="24" style="text-align: center">
+            <a-button @click="submitForm">提 交</a-button>
+          </a-col>
+        </a-row>
+      </a-form>
+    </j-form-container>
+  </a-spin>
+</template>
+
+<script>
+
+  import { httpAction, getAction } from '@/api/manage'
+  import pick from 'lodash.pick'
+  import { validateDuplicateValue } from '@/utils/util'
+  import JFormContainer from '@/components/jeecg/JFormContainer'
+  import JEditor from '@/components/jeecg/JEditor'
+
+  export default {
+    name: 'ZqQuestionContentForm',
+    components: {
+      JFormContainer,
+      JEditor,
+    },
+    props: {
+      //流程表单data
+      formData: {
+        type: Object,
+        default: ()=>{},
+        required: false
+      },
+      //表单模式：true流程表单 false普通表单
+      formBpm: {
+        type: Boolean,
+        default: false,
+        required: false
+      },
+      //表单禁用
+      disabled: {
+        type: Boolean,
+        default: false,
+        required: false
+      }
+    },
+    data () {
+      return {
+        form: this.$form.createForm(this),
+        model: {},
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        validatorRules: {
+          typeId: {
+            rules: [
+              { required: true, message: '请输入问题类型!'},
+            ]
+          },
+        },
+        url: {
+          add: "/om/zqQuestionContent/add",
+          edit: "/om/zqQuestionContent/edit",
+          queryById: "/om/zqQuestionContent/queryById"
+        }
+      }
+    },
+    computed: {
+      formDisabled(){
+        if(this.formBpm===true){
+          if(this.formData.disabled===false){
+            return false
+          }
+          return true
+        }
+        return this.disabled
+      },
+      showFlowSubmitButton(){
+        if(this.formBpm===true){
+          if(this.formData.disabled===false){
+            return true
+          }
+        }
+        return false
+      }
+    },
+    created () {
+      //如果是流程中表单，则需要加载流程表单data
+      this.showFlowData();
+    },
+    methods: {
+      add () {
+        this.edit({});
+      },
+      edit (record) {
+        this.form.resetFields();
+        this.model = Object.assign({}, record);
+        this.visible = true;
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model,'proposer','brief','content','solution','solutionState','typeId'))
+        })
+      },
+      //渲染流程表单数据
+      showFlowData(){
+        if(this.formBpm === true){
+          let params = {id:this.formData.dataId};
+          getAction(this.url.queryById,params).then((res)=>{
+            if(res.success){
+              this.edit (res.result);
+            }
+          });
+        }
+      },
+      submitForm () {
+        const that = this;
+        // 触发表单验证
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if(!this.model.id){
+              httpurl+=this.url.add;
+              method = 'post';
+            }else{
+              httpurl+=this.url.edit;
+               method = 'put';
+            }
+            let formData = Object.assign(this.model, values);
+            console.log("表单提交数据",formData)
+            httpAction(httpurl,formData,method).then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+            })
+          }
+         
+        })
+      },
+      popupCallback(row){
+        this.form.setFieldsValue(pick(row,'proposer','brief','content','solution','solutionState','typeId'))
+      },
+    }
+  }
+</script>
